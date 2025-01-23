2869a4ff3ca5e3509bd606f8c1033893c5fc81c7
Pau Lorca <2023_pau.lorca@iticbcn.cat>
2025-01-23 19:37:25 +0100

    05-sincronitzacio: activitat acabada amb blocs synchronized

--- 05-syncronized/src/Soci.java
+++ 05-syncronized/src/Soci.java
@@ -24,14 +24,20 @@ public class Soci extends Thread {
     public void run() {
         for (int i = 0; i < maxAnys; i++) {
             for (int j = 1; j <= 12; j++) {
-                float saldo = compte.getSaldo();
-                saldo += (j % 2 == 0) ? this.aportacio : (this.aportacio * -1);
+                if (j % 2 == 0) {
+                    synchronized(compte) {
+                        compte.setSaldo(compte.getSaldo() + aportacio);
+                    }
+                } else {
+                    synchronized(compte) {
+                        compte.setSaldo(compte.getSaldo() - aportacio);
+                    }
+                }
                 try {
                     sleep(rnd.nextInt(esperaMax));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                compte.setSaldo(saldo);
             }
         }
     }
