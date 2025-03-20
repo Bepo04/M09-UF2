91eb040bd0f7ed24d0d1538ea2c0fd562ffa3fe5
Pau Lorca <pau.lorca.moreso@gmail.com>
2025-03-20 17:11:13 +0100

    afegim apunts al repositori

---
+++ apunts/prova/Filosof.java
@@ -0,0 +1,12 @@
+package apunts.prova;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Filosof {
+
+    public static void main(String[] args) {
+        4
+    }
+
+}
